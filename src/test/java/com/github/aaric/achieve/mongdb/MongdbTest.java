62d466e1fbac3629256a917599c2960741fcd980
aaric <vipaaric@gmail.com>
2018-05-09 12:06:26 +0800

    0.0.1-SNAPSHOT init

---
+++ src/test/java/com/github/aaric/achieve/mongdb/MongdbTest.java
@@ -0,0 +1,17 @@
+package com.github.aaric.achieve.mongdb;
+
+import org.junit.Test;
+
+/**
+ * MongdbTest
+ *
+ * @author Aaric, created on 2018-05-09T12:00.
+ * @since 0.0.1-SNAPSHOT
+ */
+public class MongdbTest {
+
+    @Test
+    public void testPrint() {
+        System.out.println("hello");
+    }
+}
