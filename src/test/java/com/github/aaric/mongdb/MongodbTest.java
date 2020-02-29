e1277860101b309aa7f5fbddf1b1cc59054363be
aaric <qlhan@incarcloud.com>
2020-02-29 22:18:44 +0800

    0.1.0-SNAPSHOT gradle dsl

--- src/test/java/com/github/aaric/mongdb/MongodbTest.java
+++ src/test/java/com/github/aaric/mongdb/MongodbTest.java
@@ -2,7 +2,9 @@ package com.github.aaric.mongdb;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -14,6 +16,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class MongodbTest {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Test
     public void testPrint() {
